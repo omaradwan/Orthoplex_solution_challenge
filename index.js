40b4633a0cc51e37b8529ea653a101a8f2b48758
omaradwan <xomaradwanx@icloud.com>
2025-01-30 02:49:30 +0200

    apply env attributes

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const exp = require("constants");
 const express = require("express");  
+const compression = require('compression');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+require('dotenv').config(); 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // Limit each IP to 100 requests pre 15 min
+});
 
 
 const authRoute = require("./routes/authRoute.js")
 const userRoute = require("./routes/userRoute.js")
 
 
-require('dotenv').config();  
+
 
 
 const app = express();
+app.use(cors());  
+app.use(compression())
 app.use(express.json());
+app.use(limiter); 
 
 app.use('/api/auth', authRoute)
 app.use('/api/user', userRoute)
@@ -33,7 +44,7 @@ app.use((err, req, res, next) => {
 });
 
 
-
-app.listen(8000,()=>{
-    console.log("server is running on port 8000");
+let port=process.env.PORT || 3030
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
 })
